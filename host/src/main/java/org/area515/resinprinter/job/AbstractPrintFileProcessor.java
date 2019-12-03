2a93233f81858045a89454b6cc0af0d5df25bea2
Paul Reeves <paul.reeves_pc@outlook.com>
2019-12-03 12:55:00 +0000

    15.2

--- host/src/main/java/org/area515/resinprinter/job/AbstractPrintFileProcessor.java
+++ host/src/main/java/org/area515/resinprinter/job/AbstractPrintFileProcessor.java
@@ -598,7 +598,7 @@ public abstract class AbstractPrintFileProcessor<G,E> implements PrintFileProces
 		logger.trace("Writing applyImageTransforms2AfterFill:{}", () -> Log4jUtil.logImage(imageToRenderAfterTransformations, "applyImageTransforms2AfterFill.png"));
 		
 		AffineTransform transform = aid.getAffineTransform(engineForManipulation, imageToRenderAfterTransformations, imageToRender);//Leaks??
-		graphicsAfterTransformations.drawImage(imageToRender, transform, null);//leaks??15.1
+		//15.2graphicsAfterTransformations.drawImage(imageToRender, transform, null);//leaks??15.1
 	//-----leaks 15
 		// logger.trace("Writing applyImageTransforms3AfterDraw:{}", () -> Log4jUtil.logImage(imageToRenderAfterTransformations, "applyImageTransforms3AfterDraw.png"));
 
--- host/src/main/java/org/area515/resinprinter/job/CreationWorkshopSceneFileProcessor.java
+++ host/src/main/java/org/area515/resinprinter/job/CreationWorkshopSceneFileProcessor.java
@@ -192,7 +192,7 @@ public class CreationWorkshopSceneFileProcessor extends AbstractPrintFileProcess
 					 		newImage = applyImageTransforms(aid, data.getScriptEngine(), newImage);//14.2
 					// // 		// applyBulbMask(aid, (Graphics2D)newImage.getGraphics(), newImage.getWidth(), newImage.getHeight());
 					// // 		///PXR data.setPrintableImage(newImage);
-							logger.info("Show picture pxr old 0.0.15.1 : {}", imageFilename);
+							logger.info("Show picture pxr old 0.0.15. : {}", imageFilename);
 							
 							//Notify the client that the printJob has increased the currentSlice
 							NotificationManager.jobChanged(printer, printJob);
