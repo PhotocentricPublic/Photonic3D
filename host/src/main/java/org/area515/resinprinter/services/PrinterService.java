c887e658a96d6226a6cf8940d52df9b194f6edd5
Kalvis Gredzens <kalvis@photocentric.local>
2019-12-19 07:57:57 +0000

    updates up to 8ca70e0

--- host/src/main/java/org/area515/resinprinter/services/PrinterService.java
+++ host/src/main/java/org/area515/resinprinter/services/PrinterService.java
@@ -221,7 +221,7 @@ public class PrinterService {
     @ApiResponses(value = {
             @ApiResponse(code = 200, response=MachineResponse.class, message = SwaggerMetadata.MACHINE_RESPONSE),
             @ApiResponse(code = 500, message = SwaggerMetadata.UNEXPECTED_ERROR)})
-	@POST
+	@GET
 	@Path("save")
 	@Produces(MediaType.APPLICATION_JSON)
 	@Consumes(MediaType.APPLICATION_JSON)
